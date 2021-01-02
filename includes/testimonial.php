5f469256dfa7fb22e892c62da85627fe91bae358
František Gič <mcfreddie777@gmail.com>
2021-01-02 15:54:59 +0100

    Create and render testimonial custom type

---
+++ includes/testimonial.php
@@ -0,0 +1,43 @@
+<?php
+
+add_action('init', function () {
+    $labels = array(
+        'name' => __('Testimonials', 'pokymedia'),
+        'singular_name' => __('Testimonial', 'pokymedia'),
+        'menu_name' => __('Testimonials', 'pokymedia'),
+        'name_admin_bar' => __('Testimonial', 'pokymedia'),
+        'add_new' => __('Add New', 'pokymedia'),
+        'add_new_item' => __('Add New Testimonial', 'pokymedia'),
+        'new_item' => __('New Testimonial', 'pokymedia'),
+        'edit_item' => __('Edit Testimonial', 'pokymedia'),
+        'view_item' => __('View Testimonial', 'pokymedia'),
+        'all_items' => __('All Testimonials', 'pokymedia'),
+        'search_items' => __('Search Testimonials', 'pokymedia'),
+        'parent_item_colon' => __('Parent Testimonial:', 'pokymedia'),
+        'not_found' => __('No Testimonials found.', 'pokymedia'),
+        'not_found_in_trash' => __('No Testimonials found in Trash.', 'pokymedia'),
+        'featured_image' => __('Customer photo', 'pokymedia'),
+        'set_featured_image' => __('Set photo', 'pokymedia'),
+        'remove_featured_image' => __('Remove photo', 'pokymedia'),
+        'use_featured_image' => __('Use as customer\'s photo', 'pokymedia'),
+        'archives' => __('Testimonial archives', 'pokymedia'),
+        'insert_into_item' => __('Insert into testimonials', 'pokymedia'),
+        'uploaded_to_this_item' => __('Uploaded to this movie', 'pokymedia'),
+        'filter_items_list' => __('Filter testimonials list', 'pokymedia'),
+        'items_list_navigation' => __('Testimonials list navigation', 'pokymedia'),
+        'items_list' => __('Testimonials list', 'pokymedia'),
+    );
+
+
+    register_post_type('testimonial', [
+        'public' => true,
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-testimonial',
+        'has_archive' => false,
+        'rewrite' => ['slug' => 'testimonials'],
+        'supports' => [
+            'title', 'thumbnail'
+        ],
+        'menu_position' => 7
+    ]);
+});
---
+++ pokymedia-types.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name: POKYmedia Custom Types
+ * Author: František Gič
+ * Author URI: https://frantisekgic.sk
+ * Description: Custom post types
+ * Version: 1.0
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Testimonial
+require_once WPMU_PLUGIN_DIR . '/includes/testimonial.php';
